39824a4ce5cdfe2c5680cc817ce18f63cb7deb51
prajwalraju <prajwalrajup@gmail.com>
2020-04-13 01:54:55 +0530

    run motor

---
+++ Run motor/src/run_motor.h
@@ -0,0 +1,25 @@
+#ifndef run_motor_h
+#define run_motor_h
+
+#include "Arduino.h"
+
+
+class run_motor
+{
+  public:
+    motor(int motor,int speedd);
+    set(int s0,int s1,int s2,int z, int pwm_left, int pwm_right );
+
+    int pool_rate = 50;
+    int m1=1,m2=2,m3=3,m4=4,c_z;
+    int forward = 1,backward = 0;
+
+  private:
+    int selectPins[3];
+    int c_pwm_left;int c_pwm_right;
+
+    const int DELAY_TIME = ((float)pool_rate/512.0)*1000;
+
+};
+
+#endif
